3831a288b8b9840abbc93982840ab8366d29306e
Khondaker Tasnia Hoque <60292796+Tasnia16@users.noreply.github.com>
2022-02-15 03:21:45 +0700

    Add files via upload

--- Data_Science_Visualizer/Regression.cpp
+++ Data_Science_Visualizer/Regression.cpp
@@ -1,7 +1,20 @@
 #include<bits/stdc++.h>
+#include<stdlib.h>
+#include<vector>
 #include"regression.h"
 using namespace std;
 void reg_input()
 {
-    cout<<"hello";
+    FILE *fp;
+    /*fp=fopen("regression.txt","w");
+    if(fp!=NULL)
+    {
+        cout<<"successfully created file" ;
+    }*/
+    if ((fp=fopen("regression.txt","r"))==NULL)
+    {
+        cout<<"Can't open the file";
+        exit(1);
+    }
+
 }
