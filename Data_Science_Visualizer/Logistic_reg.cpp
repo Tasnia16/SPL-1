e9c3a11d5b410a1a2a4b7c63f42c715845f9a045
Khondaker Tasnia Hoque <60292796+Tasnia16@users.noreply.github.com>
2022-04-04 12:24:33 +0600

    Add files via upload

--- Data_Science_Visualizer/Logistic_reg.cpp
+++ Data_Science_Visualizer/Logistic_reg.cpp
@@ -2,10 +2,13 @@
 #include"logictic_reg.h"
 using namespace std;
 
-vector<string>s1;
-vector<string>s2;
+vector<string>s1_y;
+vector<string>s2_y;
+vector<string>s1_x;
+vector<double>x_train;
 vector<int>binary;
 set<string>set1;
+const double Threshold = 0.001;
 
 void logistic_regression()
 {
@@ -21,25 +24,25 @@ void logistic_regression()
 
     int count=0;
 
-    char split[1000];
-    while(fscanf(fp_y,"%[^\n]\n",split)!=EOF)
+    char split_y[1000];
+    while(fscanf(fp_y,"%[^\n]\n",split_y)!=EOF)
     {
-        stringstream data(split);
+        stringstream data(split_y);
         string y_var;
 
         while(data>>y_var)
             {
                  //cout<<y_var;
-                s1.push_back(y_var);
+                s1_y.push_back(y_var);
                 count++;
             }
     }
     for(int i=0;i<count;i++)
-        cout<<s1[i]<<endl;
+        cout<<s1_y[i]<<endl;
 
     for (long long int i=0;i<count;i++)
     {
-        set1.insert(s1[i]);
+        set1.insert(s1_y[i]);
     }
 
     /*for(long long int i=0;i<count;i++)
@@ -56,14 +59,14 @@ void logistic_regression()
 
     for(itr=set1.begin();itr!=set1.end();itr++)
     {
-        s2.push_back(*itr);
+        s2_y.push_back(*itr);
     }
 
    // cout<<s2[0]<<" "<<s2[1]<<endl;
 
     for (long long int i=0;i<count;i++)
     {
-        if(s1[i]==s2[0])
+        if(s1_y[i]==s2_y[0])
             binary.push_back(1);
 
 
@@ -79,9 +82,49 @@ void logistic_regression()
     fclose(fp_y);
 
 
-
+//reading from x_variable
     FILE *fp_x;
+    if ((fp_x=fopen("x_variable.txt","r"))==NULL)
+    {
+        cout<<"Can't open the file";
+        cout<<endl;
+        exit(1);
+    }
 
+    int factor=0,count_x=0;   //counting factors
+
+    char split_x[1000];
+    int line=0;
+
+    while(fscanf(fp_x,"%[^\n]\n",split_x)!=EOF)
+    {
+        line++;
+        stringstream data2(split_x);
+        string x_var;
+
+        while(data2>>x_var)
+            {
+                 //cout<<y_var;
+                s1_x.push_back(x_var);
+                count_x++;
+            }
+    }
+
+     factor=count_x/line;
+   //cout<<count_x/line;
+
+   for(long long int i=0;i<s1_x.size();i++)
+    {
+        double n;
+        n=std:: stof(s1_x[i]);
+        x_train.push_back(n);
+        //cout<<n<<endl;
+    }
+
+     /*for(long long int i=0;i<x_train.size();i++)
+    {
+        cout<<x_train[i]<<endl;
+    }*/
 
 
 }
