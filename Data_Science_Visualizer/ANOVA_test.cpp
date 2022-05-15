bc345cbab00edf2c9ac5188a5b96d7b052c24469
Khondaker Tasnia Hoque <60292796+Tasnia16@users.noreply.github.com>
2022-05-16 01:44:35 +0700

    Add files via upload

--- Data_Science_Visualizer/ANOVA_test.cpp
+++ Data_Science_Visualizer/ANOVA_test.cpp
@@ -6,10 +6,68 @@ struct group{
 
 string s[100];
 int cnt;
+double grp_avg=0.0;
+double bss=0;
+double wss=0;
 double arr[100];
 }grp[100];
 
 int maximum=0;
+double grand_avg=0.0;
+int total_cnt=0;
+double grand_sum=0.0;
+double BSS=0,WSS,BMS,WMS;
+double TS=0;
+double alpha;
+void calculation_anovaTable(int line)
+{
+    for(int i=0;i<line;i++)
+    {
+        double s=0;
+        for(int j=0;j<grp[i].cnt-1;j++)
+        {
+            s+=grp[i].arr[j];
+        }
+
+        grp[i].grp_avg=s/(grp[i].cnt-1);
+        //cout<<grp[i].grp_avg<<endl;
+    }
+
+     for(int i=0;i<line;i++)
+    {
+        grp[i].bss=(grp[i].cnt-1)*((grand_avg-grp[i].grp_avg)*(grand_avg-grp[i].grp_avg));
+    }
+
+    for(int i=0;i<line;i++)
+    {
+        BSS+=grp[i].bss;
+    }
+
+   // cout<<BSS<<endl;
+   BMS=BSS/(line-1);
+   //cout<<BMS<<endl;
+
+   for(int i=0;i<line;i++)
+   {
+       for(int j=0;j<grp[i].cnt-1;j++)
+       {
+           grp[i].wss+=(grp[i].arr[j]-grp[i].grp_avg)*(grp[i].arr[j]-grp[i].grp_avg);
+       }
+
+       WSS+=grp[i].wss;
+   }
+
+   //cout<<WSS;
+
+   WMS=WSS/(total_cnt-line);
+  // cout<<WMS;
+
+  TS=BMS/WMS;
+  cout<<TS<<endl;
+  cout<<"Enter the significance level  :"<<endl;
+  cin>>alpha;
+}
+
 
 void start_anova_test()
 {
@@ -71,11 +129,16 @@ void start_anova_test()
           double n;
           n=std:: stof(grp[i].s[k]);
           grp[i].arr[k-1]=n;
+          total_cnt++;
+          grand_sum+=n;
           cout<<grp[i].arr[k-1]<<" ";
       }
 
       cout<<endl;
   }
+//cout<<grand_sum;
+//cout<<total_cnt;
+grand_avg=grand_sum/total_cnt;
 
 for(int i=0;i<line;i++)
 {
@@ -104,6 +167,7 @@ for(int i=0;i<maximum;i++)
       cout<<endl;
   }
 
+   calculation_anovaTable(line);
 
 
 }
